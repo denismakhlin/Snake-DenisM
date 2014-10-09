2f9853e4d2d0aa87f6279385c70110dbb3c0d95e
Denis Makhlin <denismakhlin@gmail.com>
2014-10-09 15:02:30 -0700

    Been out for 3 days not close to finishing

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -3,3 +3,24 @@ var snake;
 var context;
 var screenWidth;
 var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(122, 250, 154)";
+    context.fillReact(0, 0, screenWidth, screenHeight);
+}
+    
